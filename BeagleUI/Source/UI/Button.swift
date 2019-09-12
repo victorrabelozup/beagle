c1008af5b2ce28c8f4b0fee1294d6a1637f63935
Daniel Tes <daniel@zup.com.br>
2019-09-12 14:11:46 -0300

    initial structure

---
+++ BeagleUI/Source/UI/Button.swift
@@ -0,0 +1,9 @@
+//
+//  Button.swift
+//  BeagleUI
+//
+//  Created by Daniel Tes on 12/09/19.
+//  Copyright © 2019 Daniel Tes. All rights reserved.
+//
+
+import Foundation
---
+++ BeagleUI/Source/UI/Text.swift
@@ -0,0 +1,9 @@
+//
+//  Text.swift
+//  BeagleUI
+//
+//  Created by Daniel Tes on 12/09/19.
+//  Copyright © 2019 Daniel Tes. All rights reserved.
+//
+
+import Foundation
