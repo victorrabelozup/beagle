2755b36e0f71f8839e0cadff5c9b79083ba31a88
Eduardo Bocato <dubocato@gmail.com>
2019-09-17 19:00:33 -0300

    Adding serialization tests.

--- Decodable.playground/Contents.swift
+++ Decodable.playground/Contents.swift
@@ -1,93 +1,106 @@
 import Foundation
 
-protocol Widget {
+public protocol Widget {}
+
+struct WidgetContainer: Widget, Codable {
+    
+    let type: String
+    let content: Widget?
+    
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case content
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decode(String.self, forKey: .type)
+        if let decodeFunction = WidgetContainer.decoders[type] {
+            let rawContent = try decodeFunction(container)
+            content = rawContent as? Widget
+        } else if let container = try? decoder.singleValueContainer().decode(WidgetContainer.self) {
+            content = container
+        } else {
+            content = nil
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        
+        if let content = self.content {
+            guard let encode = WidgetContainer.encoders[type] else {
+                let context = EncodingError.Context(codingPath: [], debugDescription: "Invalid attachment: \(type).")
+                throw EncodingError.invalidValue(self, context)
+            }
+            try encode(content, &container)
+        } else {
+            try container.encodeNil(forKey: .content)
+        }
+    }
+    
+    // MARK: Registration
+    
+    private typealias WidgetContainerDecoder = (KeyedDecodingContainer<CodingKeys>) throws -> Any
+    private typealias WidgetContainerEncoder = (Widget, inout KeyedEncodingContainer<CodingKeys>) throws -> Void
+    
+    private static var decoders: [String: WidgetContainerDecoder] = [:]
+    private static var encoders: [String: WidgetContainerEncoder] = [:]
+    
+    static func register<T: Codable>(_ type: T.Type, for typeName: String) {
+        decoders[typeName] = { container in
+            
+            return try container.decode(T.self, forKey: .content)
+        }
+        encoders[typeName] = { value, container in
+            guard let typedValue = value as? T else {
+                throw NSError()
+            }
+            try container.encode(typedValue, forKey: .content)
+        }
+    }
     
 }
 
-struct Container: Widget {
-    let body: Widget?
-    let content: Widget
-    let footer: Widget?
+public struct Container: Widget, Codable {
+    let body: WidgetContainer?
+    let content: WidgetContainer
+    let footer: WidgetContainer?
 }
 
-struct Vertical: Widget {
-    let children: [Widget]
+public struct Vertical: Widget, Codable {
+    let children: [WidgetContainer]
 }
 
-struct Text: Widget {
+public struct Text: Widget, Codable {
     let text: String
 }
 
-struct Image: Widget {
+public struct Image: Widget, Codable {
     let image: String
 }
 
-protocol ClassFamily: Decodable {
-    
-    static var discriminator: Discriminator { get }
-    
-    func getType() -> Any.Type
-}
-
-enum Discriminator: String, CodingKey {
-    case type = "type"
-}
-
-enum ComponentFamily: String, ClassFamily {
-    case container
-    case vertical
-    case text
-    case image
-    
-    static var discriminator: Discriminator = .type
-    
-    func getType() -> Any.Type {
-        switch self {
-        case .container:
-            return Container.self
-        case .vertical:
-            return Vertical.self
-        case .text:
-            return Text.self
-        case .image:
-            return Image.self
-        }
-    }
-}
-
-extension JSONDecoder {
-    
-    //    open func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable
-    
-    func decode<T: ClassFamily, U: Decodable>(family: T.Type, from data: Data) throws -> U {
-        return try self.decode(ClassWrapper<T, U>.self, from: data)
-    }
-    
-    private class ClassWrapper<T: ClassFamily, U: Decodable>: Decodable {
-        let family: T
-        let object: U?
-        
-        required init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: Discriminator.self)
-            family = try container.decode(T.self, forKey: T.discriminator)
-            if let type = family.getType() as? U.Type {
-                object = try type.init(from: decoder)
-            } else {
-                object = nil
-            }
-        }
-    }
-}
 
 let json = """
-{ "type": "container", "content": {
-"type": "text"
-} }
+{ "type": "container","content": {"type": "text","content": {"text": "some text"}}}
 """
+WidgetContainer.register(WidgetContainer.self, for: "container")
+WidgetContainer.register(WidgetContainer.self, for: "body")
+WidgetContainer.register(WidgetContainer.self, for: "footer")
+WidgetContainer.register(WidgetContainer.self, for: "content")
+WidgetContainer.register(WidgetContainer.self, for: "children")
+
+//        WidgetContainer.register(Container.self, for: "container")
+WidgetContainer.register(Text.self, for: "text")
 
 if let jsonData = json.data(using: .utf8) {
-    let decoder = JSONDecoder()
     
-    let component = try decoder.decode(ComponentFamily.self, from: jsonData)
-    print("\(component)")
+    do {
+        let value = try JSONDecoder().decode(WidgetContainer.self, from: jsonData)
+        print("\(value)")
+    } catch {
+        debugPrint("Error: \(error)")
+    }
 }
