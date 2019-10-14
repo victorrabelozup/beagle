468e4101eff748bc60b544d6169be81e17cb90d0
gabcoelho <34385764+gabcoelho@users.noreply.github.com>
2019-10-14 13:36:51 -0300

    Adding test to widget view rendering error. (#23)

---
+++ Sources/BeagleUI/BeagleFrameworkTests/Tests/Engine/Renderer/WidgetViewRenderingErrorTests.swift
@@ -0,0 +1,26 @@
+//
+//  WidgetViewRenderingErrorTests.swift
+//  BeagleFrameworkTests
+//
+//  Created by Gabriela Coelho on 14/10/19.
+//  Copyright © 2019 Daniel Tes. All rights reserved.
+//
+
+import XCTest
+@testable import BeagleUI
+
+final class WidgetViewRenderingErrorTests: XCTestCase {
+    
+    func test_localizedDescription_shouldReturnCorrectTextWhenUnableToCastWidgetToType() {
+        // Given
+        let type = "Something"
+        let expectedLocalizedDescription = "Could not cast widget to `\(type)`."
+        
+        // When
+        let error: WidgetViewRenderingError = .couldNotCastWidgetToType(type)
+        let localizedDescription = error.localizedDescription
+        
+        // Then
+        XCTAssertEqual(expectedLocalizedDescription, localizedDescription, "Expected \(expectedLocalizedDescription), but got \(localizedDescription).")
+    }
+}
