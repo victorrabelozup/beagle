b36be7d9139624d362fa61b0eb2674746ab41503
tuliopereirazup <55166462+tuliopereirazup@users.noreply.github.com>
2019-10-16 16:43:18 -0300

    Add navigation implementation (#10)
    * Add navigation
    
    * Rename Navigation to Navigator

---
+++ framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/navigation/Navigator.kt
@@ -0,0 +1,26 @@
+package br.com.zup.beagleui.framework.widget.navigation
+
+import br.com.zup.beagleui.framework.widget.core.Widget
+
+data class Event(
+    val type: EventType,
+    val value: DeeplinkURL? = null
+)
+
+data class DeeplinkURL(
+    val path: String,
+    val data: Map<String, String>? = null
+)
+
+enum class EventType {
+    OPEN_DEEP_LINK,
+    ADD_VIEW,
+    OPEN_VIEW,
+    FINISH_VIEW,
+    POP_VIEW
+}
+
+data class Navigator(
+    val event: Event,
+    val child: Widget
+)
