cec52d17d809e6b71b40f43ad7c24005a17c2a69
tuliopereirazup <55166462+tuliopereirazup@users.noreply.github.com>
2019-10-21 18:15:59 -0300

    Create CustomWidget sample (#16)

--- sample/src/main/java/br/com/zup/beagleui/app/ui/screen/HelloWorldScreen.kt
+++ sample/src/main/java/br/com/zup/beagleui/app/ui/screen/HelloWorldScreen.kt
@@ -1,13 +1,12 @@
 package br.com.zup.beagleui.app.ui.screen
 
-import br.com.zup.beagleui.framework.core.Screen
-import br.com.zup.beagleui.framework.layout.Container
-import br.com.zup.beagleui.framework.ui.Text
+import br.com.zup.beagleui.app.widget.CustomWidget
+import br.com.zup.beagleui.framework.widget.core.Screen
+import br.com.zup.beagleui.framework.widget.layout.Container
 
 class HelloWorldScreen : Screen {
     
     override fun build() = Container(
-        content = Text("Hello World")
+        content = CustomWidget()
     )
-
 }
