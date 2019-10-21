70252b80effe07af53726e2b46bfeecc6d94753f
tuliopereirazup <55166462+tuliopereirazup@users.noreply.github.com>
2019-10-21 14:20:10 -0300

    Refactor ListView logic (#14)

--- framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/ui/ListView.kt
+++ framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/ui/ListView.kt
@@ -1,8 +1,7 @@
 package br.com.zup.beagleui.framework.widget.ui
 
+import br.com.zup.beagleui.framework.widget.core.NativeWidget
 import br.com.zup.beagleui.framework.widget.core.Widget
-import br.com.zup.beagleui.framework.widget.layout.Horizontal
-import br.com.zup.beagleui.framework.widget.layout.Vertical
 
 enum class ListDirection {
     VERTICAL,
@@ -12,30 +11,31 @@ enum class ListDirection {
 typealias RowBuilder = (index: Int) -> Widget
 
 data class ListView(
+    val rows: List<Widget>,
     val remoteDataSource: String? = null,
     val loadingState: Widget? = null,
-    @Transient val size: Int,
-    @Transient val rowBuilder: RowBuilder,
-    @Transient val direction: ListDirection = ListDirection.VERTICAL
-) : Widget {
+    val direction: ListDirection = ListDirection.VERTICAL
+) : NativeWidget {
 
-    override fun build(): Widget {
-        val children = generateRows(size, rowBuilder)
-
-        return if (direction == ListDirection.VERTICAL) {
-            Vertical(children = children)
-        } else {
-            Horizontal(children = children)
-        }
-    }
-
-    private fun generateRows(size: Int, rowBuilder: RowBuilder): List<Widget> {
-        val children = mutableListOf<Widget>()
-
-        for(i in 0 until size) {
-            children.add(rowBuilder(i))
+    companion object {
+        @JvmStatic
+        fun dynamic(
+            size: Int,
+            direction: ListDirection = ListDirection.VERTICAL,
+            rowBuilder: RowBuilder
+        ): ListView {
+            val rows = generateRows(size, rowBuilder)
+            return ListView(rows = rows, direction = direction)
         }
 
-        return children
+        private fun generateRows(size: Int, rowBuilder: RowBuilder): List<Widget> {
+            val children = mutableListOf<Widget>()
+
+            for(i in 0 until size) {
+                children.add(rowBuilder(i))
+            }
+
+            return children
+        }
     }
 }
