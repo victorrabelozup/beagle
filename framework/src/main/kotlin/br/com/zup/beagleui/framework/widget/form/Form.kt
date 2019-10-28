4c161fddcb72ea92fc21a0db4d01b6171f213a58
tuliopereirazup <55166462+tuliopereirazup@users.noreply.github.com>
2019-10-28 14:17:33 -0300

    Improve Forms attributes (#19)
    * Improve Forms attributes
    
    * Add put and delete methods

--- framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/form/Form.kt
+++ framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/form/Form.kt
@@ -3,6 +3,15 @@ package br.com.zup.beagleui.framework.widget.form
 import br.com.zup.beagleui.framework.widget.core.NativeWidget
 import br.com.zup.beagleui.framework.widget.core.Widget
 
+enum class FormMethodType {
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
 data class Form (
+    val action: String,
+    val method: FormMethodType,
     val child: Widget
 ) : NativeWidget
--- framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/form/FormSubmit.kt
+++ framework/src/main/kotlin/br/com/zup/beagleui/framework/widget/form/FormSubmit.kt
@@ -4,6 +4,5 @@ import br.com.zup.beagleui.framework.widget.core.NativeWidget
 import br.com.zup.beagleui.framework.widget.core.Widget
 
 data class FormSubmit(
-    val url: String,
     val child: Widget
 ) : NativeWidget
