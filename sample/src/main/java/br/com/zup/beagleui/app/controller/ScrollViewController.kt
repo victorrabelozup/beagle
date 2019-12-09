10fa2e7c8e5204179504c146a40135916b98beaf
Matheus Ribeiro Lima <57918064+matheusribeirozup@users.noreply.github.com>
2019-12-09 10:30:35 -0300

    added endpoint to return a scroll widget (#60)

---
+++ sample/src/main/java/br/com/zup/beagleui/app/controller/ScrollViewController.kt
@@ -0,0 +1,52 @@
+package br.com.zup.beagleui.app.controller
+
+import br.com.zup.beagleui.framework.widget.core.Flex
+import br.com.zup.beagleui.framework.widget.core.FlexDirection
+import br.com.zup.beagleui.framework.widget.core.Widget
+import br.com.zup.beagleui.framework.widget.layout.FlexWidget
+import br.com.zup.beagleui.framework.widget.layout.ScrollAxis
+import br.com.zup.beagleui.framework.widget.layout.ScrollView
+import br.com.zup.beagleui.framework.widget.ui.Text
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ScrollViewController {
+
+    @RequestMapping("/scrollView")
+    fun getPageView(): Widget {
+        return ScrollView(
+            scrollDirection = ScrollAxis.HORIZONTAL,
+            children = listOf(
+                FlexWidget(
+                    flex = Flex(
+                        flexDirection = FlexDirection.ROW
+                    ),
+                    children = listOf(
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto"),
+                        Text("Texto")
+                    )
+                )
+            )
+        )
+    }
+}
