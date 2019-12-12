1109810aa5cd14c97abfc507d9a65c9aac0dbee8
Matheus Ribeiro Lima <57918064+matheusribeirozup@users.noreply.github.com>
2019-12-12 11:39:27 -0300

    added aligment attribute to text widget (#70)

--- src/main/kotlin/br/com/zup/beagleui/framework/widget/ui/Text.kt
+++ src/main/kotlin/br/com/zup/beagleui/framework/widget/ui/Text.kt
@@ -4,5 +4,12 @@ import br.com.zup.beagleui.framework.widget.core.Widget
 
 data class Text(
     val text: String,
-    val style: String? = null
+    val style: String? = null,
+    val alignment: TextAlignment? = null
 ) : Widget
+
+enum class TextAlignment {
+    LEFT,
+    CENTER,
+    RIGHT
+}
