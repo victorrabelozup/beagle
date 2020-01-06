857771bd5989e252ea9beb5058f73b424a29b9e6
Danilo Rocha <42280824+danilorochazup@users.noreply.github.com>
2020-01-06 16:33:47 -0300

    create form submit enable (#83)
    * create form submit enable
    
    * fix enabled name
    
    * [maven-release-plugin] prepare release beagle-0.0.22
    
    * [maven-release-plugin] prepare for next development iteration

--- beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/form/FormSubmit.kt
+++ beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/form/FormSubmit.kt
@@ -3,5 +3,6 @@ package br.com.zup.beagle.widget.form
 import br.com.zup.beagle.widget.core.Widget
 
 data class FormSubmit(
-    val child: Widget
+    val child: Widget,
+    val enabled: Boolean = true
 ) : Widget
