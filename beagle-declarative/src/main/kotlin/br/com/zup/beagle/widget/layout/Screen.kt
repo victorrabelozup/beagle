14830f8303bf167b6032799956d294bbfce2f8b7
Lucas Araújo <lucasaraujo@users.noreply.github.com>
2020-01-28 11:38:41 -0300

    Add safeArea to Screen (#86)

--- beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/layout/Screen.kt
+++ beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/layout/Screen.kt
@@ -2,12 +2,20 @@ package br.com.zup.beagle.widget.layout
 
 import br.com.zup.beagle.widget.core.Widget
 
+data class SafeArea(
+    val top: Boolean? = null,
+    val leading: Boolean? = null,
+    val bottom: Boolean? = null,
+    val trailing: Boolean? = null
+)
+
 data class NavigationBar(
     val title: String,
     val showBackButton: Boolean? = null
 )
 
 data class Screen(
+    val safeArea: SafeArea? = null,
     val navigationBar: NavigationBar? = null,
     val header: Widget? = null,
     val content: Widget,
