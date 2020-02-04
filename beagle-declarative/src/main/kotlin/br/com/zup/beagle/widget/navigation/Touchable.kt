439119aa92c954cb62879a856130cce29da2f7b3
Matheus Ribeiro Lima <57918064+matheusribeirozup@users.noreply.github.com>
2020-02-04 16:35:54 -0300

    changed navigator to touchable and navigate to action (#94)

--- beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/navigation/Touchable.kt
+++ beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/navigation/Touchable.kt
@@ -1,9 +1,9 @@
 package br.com.zup.beagle.widget.navigation
 
-import br.com.zup.beagle.action.Navigate
+import br.com.zup.beagle.action.Action
 import br.com.zup.beagle.widget.core.Widget
 
-data class Navigator(
-    val action: Navigate,
+data class Touchable(
+    val action: Action,
     val child: Widget
 ) : Widget
