699519928d393b514af14b513e13e5afa54d2a5b
Danilo Rocha <42280824+danilorochazup@users.noreply.github.com>
2020-02-06 17:17:52 -0300

    action added at button (#101)

--- beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/ui/Button.kt
+++ beagle-declarative/src/main/kotlin/br/com/zup/beagle/widget/ui/Button.kt
@@ -1,8 +1,10 @@
 package br.com.zup.beagle.widget.ui
 
+import br.com.zup.beagle.action.Action
 import br.com.zup.beagle.widget.core.Widget
 
 data class Button(
     val text: String,
-    val style: String? = null
+    val style: String? = null,
+    val action: Action? = null
 ) : Widget
