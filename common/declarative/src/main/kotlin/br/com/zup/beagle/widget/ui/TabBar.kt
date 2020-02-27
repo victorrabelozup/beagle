a644fce501e9cb319f93935c0734e903b5fd6e9e
tuliopereirazup <55166462+tuliopereirazup@users.noreply.github.com>
2020-02-27 18:30:49 -0300

    Delete TabBar (#40)

--- common/declarative/src/main/kotlin/br/com/zup/beagle/widget/ui/TabBar.kt
+++
@@ -1,8 +0,0 @@
-package br.com.zup.beagle.widget.ui
-
-import br.com.zup.beagle.core.ServerDrivenComponent
-
-data class TabBar(
-    val titles: List<String>,
-    val contents: List<ServerDrivenComponent>
-) : ServerDrivenComponent
