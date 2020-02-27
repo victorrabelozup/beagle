7ce7ca6215b8861356712366df2e85e398650982
Hugo Themir <hthemir@hotmail.com>
2020-02-27 16:41:41 -0300

    change input widget parent to widget (#33)
    Co-authored-by: tuliopereirazup <55166462+tuliopereirazup@users.noreply.github.com>

--- backend/backend-declarative/src/main/kotlin/br/com/zup/beagle/widget/form/InputWidget.kt
+++ backend/backend-declarative/src/main/kotlin/br/com/zup/beagle/widget/form/InputWidget.kt
@@ -1,5 +1,16 @@
 package br.com.zup.beagle.widget.form
 
-import br.com.zup.beagle.core.ServerDrivenComponent
+import br.com.zup.beagle.core.Appearance
+import br.com.zup.beagle.widget.Widget
+import br.com.zup.beagle.widget.core.Flex
 
-interface InputWidget : ServerDrivenComponent
+abstract class InputWidget : Widget() {
+
+    override fun applyAppearance(appearance: Appearance): InputWidget {
+        return super.applyAppearance(appearance) as InputWidget
+    }
+
+    override fun applyFlex(flex: Flex): InputWidget {
+        return super.applyFlex(flex) as InputWidget
+    }
+}
