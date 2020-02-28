927529c0a6c8a0f5b0dbf70c9bdfd3eb5e42678d
viniciusguardieirozup <58824112+viniciusguardieirozup@users.noreply.github.com>
2020-02-28 11:33:58 -0300

    config dokka to 0.10.1 version and config to generate markdown files (#35)
    Co-authored-by: tiagonuneszup <42281157+tiagonuneszup@users.noreply.github.com>

--- android/android-declarative/src/main/java/br/com/zup/beagle/widget/form/InputWidget.kt
+++ android/android-declarative/src/main/java/br/com/zup/beagle/widget/form/InputWidget.kt
@@ -5,13 +5,18 @@ import br.com.zup.beagle.widget.core.Flex
 import br.com.zup.beagle.widget.core.WidgetView
 
 abstract class InputWidget : WidgetView() {
+    /** @suppress */
     abstract fun getValue(): Any
+
+    /** @suppress */
     abstract fun onErrorMessage(message: String)
 
+    /** @suppress */
     override fun applyAppearance(appearance: Appearance): InputWidget {
         return super.applyAppearance(appearance) as InputWidget
     }
 
+    /** @suppress */
     override fun applyFlex(flex: Flex): InputWidget {
         return super.applyFlex(flex) as InputWidget
     }
