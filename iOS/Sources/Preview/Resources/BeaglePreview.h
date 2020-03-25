a3426771fa987d79f140e5aaa10f846768a98409
Marcelo Mendes <mpmarcelomp@gmail.com>
2020-03-25 09:29:48 -0300

    adding live preview submodule (#137)
    * adding live preview submodule
    
    * adding copyright header
    
    Co-authored-by: Marcelo Mendes <marcelomendes@MacBook-Pro-de-Marcelo-Mendes.local>
    Co-authored-by: Marcelo Mendes <marcelo@zup.com.br>

---
+++ iOS/Sources/Preview/Resources/BeaglePreview.h
@@ -0,0 +1,15 @@
+//
+//  Copyright © 11/02/20 Zup IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeaglePreview.
+FOUNDATION_EXPORT double BeaglePreviewVersionNumber;
+
+//! Project version string for BeaglePreview.
+FOUNDATION_EXPORT const unsigned char BeaglePreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeaglePreview/PublicHeader.h>
+
+
