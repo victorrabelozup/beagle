085c80585c31eeab55a77fea451014b8a22675bb
Túlio Magalhães <55166462+tuliopereirazup@users.noreply.github.com>
2020-04-16 16:43:47 -0300

    Update License to Apache 2.0 (#190)
    * Update license to apache 2.0
    
    * Update iOS License to Apache 2.0
    
    * Rollback YogaLayout license
    
    * Fix other license files

--- android/beagle/src/main/java/br/com/zup/beagle/view/YogaLayout.java
+++ android/beagle/src/main/java/br/com/zup/beagle/view/YogaLayout.java
@@ -1,10 +1,10 @@
 /*
- * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 package br.com.zup.beagle.view;
 
 import android.annotation.SuppressLint;
