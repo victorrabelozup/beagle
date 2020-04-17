28195fe94021472a138f0bd50a6e8ed667c81134
Uzias Ferreira <uzias.frreira@zup.com.br>
2020-04-17 19:01:15 -0300

    Adjust release

--- fastlane/delete_release.sh
+++ fastlane/delete_release.sh
@@ -1,29 +1,3 @@
-wget https://github.com/github/hub/releases/download/v2.7.1/hub-linux-amd64-2.7.1.tgz
-tar zvxvf hub-linux-amd64-2.7.1.tgz
-sudo ./hub-linux-amd64-2.7.1/install
-# Setup autocomplete for bash:
-mkdir -p ~/.bash/completions
-mv ./hub-linux-amd64-2.7.1/etc/hub.bash_completion.sh ~/.bash/completions/_hub
-echo "if [ -f ~/.bash/completions/_hub ]; then" >> ~/.bashrc
-echo "    . ~/.bash/completions/_hub" >> ~/.bashrc
-echo "fi" >> ~/.bashrc
-# add alias
-echo "eval "$(hub alias -s)"" >> ~/.bashrc
-# Cleanup
-rm -rf hub-linux-amd64-2.7.1
-hub version
-set -e
-# Authorize hub
-mkdir -p ~/.config/
-cat << EOF > ~/.config/hub
-github.com:
-  - user: "$NAME_REPO_TOKEN"
-    oauth_token: "$REPO_TOKEN"
-    protocol: https
-EOF
-set +e
-##cd $PROJECT_LOCATION
-pwd
 ## errors if release doesn't exist
 echo "${VERSION_DEPLOY}"
 
