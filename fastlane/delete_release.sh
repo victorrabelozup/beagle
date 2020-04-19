42e33c027fa93c2b3814196dd1a9c4e0810d35e4
Uzias Ferreira <uzias.frreira@zup.com.br>
2020-04-19 20:56:37 -0300

    Adjust sh

--- fastlane/delete_release.sh
+++ fastlane/delete_release.sh
@@ -1,5 +1,1 @@
-## errors if release doesn't exist
-echo "${VERSION_DEPLOY}"
-
-git tag -d ${VERSION_DEPLOY}
-git push "https://$NAME_REPO_TOKEN:$REPO_TOKEN@github.com/ZupIT/beagle-closed.git" :${VERSION_DEPLOY}
+hub release delete ${VERSION_DEPLOY}
